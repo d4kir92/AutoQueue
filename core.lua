e7359f2e64b834de8a0f43b715436217679ff724
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-12-10 09:13:04 +0100

    v1.0.14

--- core.lua
+++ core.lua
@@ -167,7 +167,7 @@ AutoQueue:OnEvent(
 	function()
 		AutoQueue:UnregisterEvent(auf, "PLAYER_LOGIN")
 		AutoQueue:SetAddonOutput("AutoQueue", 136056)
-		AutoQueue:SetVersion(136056, "1.0.13")
+		AutoQueue:SetVersion(136056, "1.0.14")
 		if AQTAB == nil then
 			AQTAB = AQTAB or {}
 		end
