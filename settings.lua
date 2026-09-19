d4a2d2ebc630bfcf3f76f8b03c13d01741bbb32c
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-19 17:17:39 +0200

    v1.1.2

--- settings.lua
+++ settings.lua
@@ -49,7 +49,7 @@ end
 
 function AutoQueue:InitSettings()
 	AQTAB = AQTAB or {}
-	AutoQueue:SetVersion(136056, "1.1.1")
+	AutoQueue:SetVersion(136056, "1.1.2")
 	AutoQueue:AddSlash("aq", AutoQueue.ToggleSettings)
 	AutoQueue:AddSlash("autoqueue", AutoQueue.ToggleSettings)
 	aqset = AutoQueue:CreateUIWindow({
